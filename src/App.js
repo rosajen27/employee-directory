b5194cbdd815df18b96e8e8b702068311d92e24a
Jennifer Rosa <rosajen27@gmail.com>
2020-08-30 20:02:14 -0400

    insert Empty Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import EmptyContainer from './components/EmptyContainer';
 
 function App() {
-  return ;
+  return <EmptyContainer/>;
   // (
   //   <div className="App">
   //     <header className="App-header">
